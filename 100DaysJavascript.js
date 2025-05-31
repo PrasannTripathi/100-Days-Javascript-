cf569cdb52a670749bb11261bc0931130a640905
prasann <prasanntripathi83050@gmail.com>
2025-05-31 23:15:35 +0530

    3rd day commit

--- 100DaysJavascript.js
+++ 100DaysJavascript.js
@@ -62,3 +62,47 @@
 // console.log(generateHash("this is a hashtag generator"));
 
 //***************************************2nd Question end here******************* */
+
+// Write a function called countCharacter that takes two parameters: a string (str) and a character (char). The function should return the number of times the specified character appears in the string.
+
+// const countCharacter = (str, char) => {
+//   let count = 0;
+//   for (let i = 0; i < str.length; i++) {
+//     if (str[i] === char) {
+//       count++;
+//     }
+//   }
+//   return count;
+// };
+// console.log(countCharacter("banana", "a"));
+
+
+// Method 2nd ////
+
+// const countCharacter2 = (str,char) => {
+//  return str.split(char).length -1
+// }
+
+// console.log(countCharacter2("tomato", "t"));
+
+//*****************3rd Question end here ***************************************** */
+
+// Write a function checkTriangleType that takes three parameters representing the lengths of the sides of a triangle. The function should return a string indicating the type of triangle: 'Equilateral', 'Isosceles', or 'Scalene'. If the given sides cannot form a valid triangle, the function should return 'Not a valid triangle'.
+
+// const checkTriangleType = (a, b, c) => {
+//   if (a + b <= c || a + c <= b || b + c <= a) return "Not a valid triangle";
+
+//   if (a === b && b === c) {
+//     return "Equilateral";
+//   } else if (a === b || b === c || a === c) {
+//     return "Isosceles";
+//   } else {
+//     return "Scalene";
+//   }
+// };
+
+// console.log(checkTriangleType(1, 1, 1)); 
+// console.log(checkTriangleType(2, 2, 3)); 
+// console.log(checkTriangleType(3, 4, 5)); 
+
+//************************ 4th question end here************************* */
